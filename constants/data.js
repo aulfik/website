cd29811b074b0e82b9e2869eb3d5f5630393949b
Aulia Fikri <auautori@gmail.com>
2022-04-14 11:43:35 +0700

    First commite

---
+++ constants/data.js
@@ -0,0 +1,103 @@
+const userData = {
+  githubUsername: "aulfik",
+  name: "Aulia Fikri",
+  designation: "Junior Data Scientist",
+  avatarUrl: "/avatar.png",
+  email: "auautori@gmail.com",
+  phone: "+628 2273531827",
+  address: "Indonesia.",
+  projects: [
+    {
+      title: "None",
+      link: "https://github.com/aulfik/#",
+      imgUrl: "/-.jpg",
+    },
+    {
+      title: "None",
+      link: "https://github.com/aulfik/#",
+      imgUrl: "/-.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://github.com/aulfik/#",
+      imgUrl: "/-.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://github.com/aulfik/#",
+      imgUrl: "/-.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://github.com/aulfik/#",
+      imgUrl: "/-.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://github.com/aulfik/#",
+      imgUrl: "/-.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://github.com/aulfik/#",
+      imgUrl: "/-.png",
+    },
+    {
+      title: "Portfolio",
+      link: "hhttps://github.com/aulfik/#",
+      imgUrl: "/-.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a Bachelor Engineer, Data Scientits",
+    description: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent odio nibh, bibendum ac mi quis, blandit euismod magna. Aenean quis hendrerit leo, in pharetra leo. Donec vel metus auctor, ullamcorper tortor id, fringilla nunc. Praesent laoreet enim et varius consequat. Nulla eu condimentum dolor. Donec posuere libero in tellus placerat cursus. Pellentesque finibus fringilla dignissim.',
+      'Vivamus interdum pharetra sapien, quis porta ante accumsan vel. Quisque semper auctor ipsum et euismod. Quisque gravida sed tellus eu ornare. Curabitur vitae pulvinar quam. Integer fermentum odio eu mollis suscipit. In tincidunt non lorem vel mollis. Sed commodo, libero id porta tempus, lorem lacus sodales massa, at dignissim nisl massa nec neque.',
+      'Curabitur ultricies gravida orci. Phasellus orci nulla, accumsan ut sem ut, viverra aliquam nunc. Suspendisse mauris dolor, consequat nec magna in, porttitor varius purus. Nunc faucibus feugiat bibendum. Cras interdum augue nunc, id auctor mi fermentum et. Nullam mauris nisl, efficitur sed dui eget, semper bibendum diam. Donec fermentum semper mauris quis tincidunt. Aenean cursus velit sed finibus porttitor. Donec vel tortor vel est luctus lobortis. Aenean est est, luctus ut nisi at, eleifend laoreet tellus. Aliquam pulvinar, ante molestie tempus suscipit, leo dui pretium elit, sagittis eleifend mauris lectus suscipit dolor. Maecenas convallis ac nunc a ultricies. Aliquam feugiat et justo sed vehicula. Nulla id rhoncus purus, sodales pretium erat.',
+    ],
+    currentProject: "On Progress",
+    currentProjectUrl: "https://github.com/aulfik/#",
+  },
+  blog: [
+    {
+      title: "Ungraduate",
+      company: "Magister University of Lampung",
+      year: "2021",
+      companyLink: "https://github.com/aulfik/#",
+      desc: "",
+    },
+    {
+      title: "Bachelor Degree",
+      company: "UNiversity of Lampung",
+      year: "2021",
+      companyLink: "https://github.com/aulfik/#",
+      desc: "",
+    },
+    {
+      title: "Hig School",
+      company: "SMA N 1 Kalirejo",
+      year: "2021",
+      companyLink: "https://github.com/aulfik/#",
+      desc: "",
+    },
+    {
+      title: "Internship",
+      company: "Wijaya Karya Persero (Tbk)",
+      year: "2015",
+      companyLink: "https://github.com/aulfik/#",
+      desc: "",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com",
+  socialLinks: {
+    instagram: "https://instagram.com/aul.fik",
+    twitter: "https://twitter.com/FAKteng",
+    linkedin: "https://linkedin.com/in/aulia-fikri",
+    github: "https://github.com/aulfik",
+    facebook: "https://facebook.com/fakteng",
+  },
+};
+
+export default userData;
---
+++ pages/blog.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import Blog from "../components/Blog";
+
+export default function experience() {
+  return (
+    <ContainerBlock title="Blog - Aulia Fikri">
+      <Blog />
+    </ContainerBlock>
+  );
+}
