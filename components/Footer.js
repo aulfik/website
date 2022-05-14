203dcc57ea6d1bcecffe7ea84f31d3818f3f5f7a
Aulia Fikri <aulia.fikri101219@students.unila.ac.id>
2022-05-14 17:37:45 +0700

    Footer fix

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
     <div className="bg-[#F1F1F1] dark:bg-gray-900">
       <div className="max-w-5xl mx-auto px-4 py-10 md:py-4">
         <div className="h-0.5 w-full bg-white dark:bg-gray-700"></div>
-        <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row justify-between md:items-center mt-8">
+        <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row justify-between md:items-center mt-8 mb-8">
           <div>
             <p>2022. Aulia Fikri.</p>
           </div>
