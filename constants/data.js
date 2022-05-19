3e632ad3b0a98794eded56717238a57a34e07ec1
Aulia Fikri <aulia.fikri101219@students.unila.ac.id>
2022-05-19 21:36:55 +0700

    add some project

--- constants/data.js
+++ constants/data.js
@@ -14,8 +14,13 @@ const userData = {
     },
     {
       title: "Github Project Portfolio",
-      link: "hhttps://github.com/aulfik/",
+      link: "https://github.com/aulfik/",
       imgUrl: "/SCR-20220514-kgj.png",
+    },
+    {
+      title: "lcs License Generator (Golang)",
+      link: "https://github.com/aulfik/lcs",
+      imgUrl: "/SCR-20220519-trw.png",
     },
   ],
   about: {
