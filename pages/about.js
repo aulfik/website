ad3a4f4fc4272a570e7d6ac19fe42c8d0be0c0b7
Aulia Fikri <aulia.fikri101219@students.unila.ac.id>
2022-06-12 12:19:26 +0700

    "edit minor fix"

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ import AboutMe from "../components/AboutMe";
 
 export default function about() {
   return (
-    <ContainerBlock>
+    <ContainerBlock title="About - Aulia Fikri">
       <AboutMe />
     </ContainerBlock>
   );
